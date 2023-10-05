d8a5cc4eeefadc5bc9b0f05e4e69ed8c2322c5fa
hugo <hugo.mercat@epita.fr>
2023-10-05 16:14:18 +0200

    remove the tailwindcss dependency for the final non-dev version

--- snippets/customizable-layout.php
+++ snippets/customizable-layout.php
@@ -1,35 +1,28 @@
-<?= css('media/plugins/pixelopen/customizable-layout/styles.css') ?>
+<?= css('media/plugins/pixelopen/customizable-layout/styles.css')?>
 
-<?php foreach ($field->toLayouts() as $layout) : ?>
-    <?php $classColumnChild = []; ?>
-    <?php $layout_settings = $layout->attrs()?>
-    <?php foreach ($layout->columns() as $column) : ?>
-        <?php
+<?php
+    foreach ($field->toLayouts() as $layout) :
+    $classColumnChild = [];
+    $layout_settings = $layout->attrs();
+    foreach ($layout->columns() as $column) {
         $classColumnChild[] = str_replace('/', '', $column->width());
         $classColumns = implode("-", $classColumnChild);
-        ?>
-    <?php endforeach ?>
-
-<section 
-    class=  "[&_*]:text-<?= $layout_settings->text_color() ?>
-            bg-<?= $layout_settings->background() ?>
-            flex
-            justify-<?= $layout_settings->x_align() ?>"
-    
-    <?php if ($layout_settings->image()->toFile()) : ?>
-        style=  "background-image: url(<?= $layout_settings->image()->toFile()->url() ?>);
-                background-position: center center;
-                background-size: cover;"
-    <?php endif ?>
-
-    id=     "<?= esc($layout->id(), 'attr') ?>" 
->
-    
-    <?php $col_disp = "";
+    }
+?>
+    <section class="flex justify-<?= $layout_settings->x_align()
+    ?>" style="<?php if ($layout_settings->image()->toFile()) {
+        echo "background-image: url(" . $layout_settings->image()->toFile()->url() . "); background-position: center center; background-size: cover;";
+    }
+    ?><?php if ($layout_settings->background() != "") {
+                echo "background-color: " . $layout_settings->background();
+            }
+    ?>" id="<?= esc($layout->id(), 'attr') ?>">
+      <?php
+    $col_disp = "";
     foreach ($layout->columns() as $column) {
         $isSetting = false;
         foreach ($column->blocks() as $block) {
-            if ($block->type() == "block-settings") {
+            if ($block->type() == "settings") {
                 $col_disp .= $block->width() . "fr ";
                 $isSetting = true;
                 break;
@@ -39,106 +32,82 @@
             $col_disp .= "3fr ";
         }
     }
-    ?>
-
-    <div
-        class=  "<?= esc($layout_settings->class(), 'attr') ?>
-                bloc__columns__<?= $classColumns; ?>
-                layout
-                py-8
-                grid
-                max-w-7xl
-                h-full
-                gap-<?= $layout_settings->column_gap() ?>
-                w-<?= $layout_settings->width() ?>"
-                
-        id=     "<?= esc($layout_settings->id(), 'attr') ?>" 
-
-        style=  "grid-template-columns: <?= $col_disp ?>;"
-        
-        <?php foreach ($layout_settings->toArray() as $key => $value) :
-            if ($value != "" && str_contains($key, "data-aos")) {
-                echo " $key=\"$value\" ";
+    ?><div class="<?= esc($layout_settings->class(), 'attr') ?> bloc__columns__<?= $classColumns; ?> layout py-8 grid max-w-7xl h-full gap-<?= $layout_settings->column_gap() ?> w-<?= $layout_settings->width()
+    ?>" id="<?= esc($layout_settings->id(), 'attr')
+    ?>" style="grid-template-columns: <?= $col_disp ?>;" <?php
+        foreach ($layout_settings->toArray() as $key => $value) :
+            if ($value != "" && $value != 0 && $value != "false" && str_contains($key, "data-aos")) {
+                echo "$key=\"$value\" ";
             }
         endforeach;
-    ?>
-    >
-
+    ?>>
     <?php foreach ($layout->columns() as $column) : ?>
-        <div
-            <?php $settingsSet = false;
+    <div <?php
+        $settingsSet = false;
         $setting_block = null;
         foreach ($column->blocks() as $block) {
             if (! $settingsSet && $block->type() == "block-settings") {
                 $setting_block = $block;
                 foreach ($block->content()->toArray() as $key => $value) {
-                    if ($value != "" && str_contains($key, "data-aos")) {
-                        echo " $key=\"$value\" ";
+                    if ($value != "" && $value != 0 && $value != "false" && str_contains($key, "data-aos")) {
+                        echo "$key=\"$value\" ";
                     }
-                    if ($key == "x_align") {
-                        echo " align=\"$value\" ";
+                    if ($value != "" && $key == "x_align") {
+                        echo "align=\"$value\" ";
                     }
                 }
             }
         }
-        ?>
+        ?>class="column prose max-w-7xl flex flex-wrap column__<?= esc($column->span(), 'css') ?><?php
+                if ($setting_block != null) {
+                    echo "" . esc($setting_block->class(), 'attr') . " ";
+                    echo "content-" . esc($setting_block->y_align(), 'attr') . " ";
 
-            class=  "column
-                    prose 
-                    max-w-7xl
-                    flex
-                    flex-wrap
-                    column__<?= esc($column->span(), 'css') ?>
-                    <?php if ($setting_block != null) {
-                        echo "" . esc($setting_block->class(), 'attr') . " ";
-                        echo "bg-" . esc($setting_block->background(), 'attr') . " ";
-                        echo "[&_*]:text-" . esc($setting_block->text_color(), 'attr') . " ";
-                        echo "content-" . esc($setting_block->y_align(), 'attr') . " ";
-
-                        if (count($layout->columns()) > 1) {
-                            if ($setting_block->image()->toFile() || $setting_block->background() != "") {
-                                echo "p-" . $setting_block->margin() . " ";
-                            } elseif ($column == $layout->columns()->first()) {
-                                echo "pr-" . $setting_block->margin() . " ";
-                            } elseif ($column == $layout->columns()->last()) {
-                                echo "pl-" . $setting_block->margin() . " ";
-                            } else {
-                                echo "px-" . $setting_block->margin() . " ";
-                            }
+                    if (count($layout->columns()) > 1) {
+                        if ($setting_block->image()->toFile() || $setting_block->background() != "") {
+                            echo "p-" . $setting_block->margin() . " ";
+                        } elseif ($column == $layout->columns()->first()) {
+                            echo "pr-" . $setting_block->margin() . " ";
+                        } elseif ($column == $layout->columns()->last()) {
+                            echo "pl-" . $setting_block->margin() . " ";
+                        } else {
+                            echo "px-" . $setting_block->margin() . " ";
                         }
-                    }?>"
+                    }
+                }
+            ?>" <?php
+            if ($setting_block != null) :
+            ?>id="<?= ($setting_block != null ? esc($setting_block->id(), 'attr') : "")
+            ?>" style="<?php
+                if ($setting_block->image()->toFile()) {
+                    echo "background-image: url(" . $setting_block->image()->toFile()->url() . "); background-position: center center; background-size: cover;";
+                }
+                if ($setting_block->background() != "") {
+                    echo "background-color: " . $setting_block->background();
+                }
+        ?>"<?php endif
+        ?>>
+          <?php
+            foreach ($column->blocks() as $block) :
+                if ($block->type() != "settings") :
+        ?><div class="prose [&>p]:my-0 flex-none max-w-none shrink basis-full<?php if ($setting_block != null) {
+                            if ($block != $column->blocks()->last() && $block->next()->type() != "settings") {
+                                echo " " . $setting_block->blocksSpace(); }}
+                    ?>"><?php
+                    $color_style = $setting_block != null && $setting_block->text_color() != "" ? " style=\"color: " . $setting_block->text_color() . "\"" : ($layout_settings->text_color() != "" ? " style=\"color: " . $layout_settings->text_color() . "\"" : "");
+                    $uri = preg_replace("/<figcaption/", "<figcaption" . $color_style ,preg_replace("/\s+/", " ", $block->toHtml()));
+                    $pattern = '{<(.*)( .*)?>(.*)<\/\1>}';
 
-            id=     "<?= ($setting_block != null ? esc($setting_block->id(), 'attr') : "") ?>"
-
-            <?php if ($setting_block && $setting_block->image()->toFile()) : ?>
-                style=  "background-image: url(<?= $setting_block->image()->toFile()->url() ?>);
-                        background-position: center center;
-                        background-size: cover;"
-            <?php endif ?>
-        >
-            <?php foreach ($column->blocks() as $block) : ?>
-                <?php if ($block->type() != "block-settings") : ?>
-            <div
-                class=  "prose
-                        [&>p]:my-0
-                        flex-none
-                        max-w-none
-                        shrink
-                        basis-full
-                        <?php if ($setting_block != null) : ?>
-                            <?php if ($block != $column->blocks()->last() && $block->next()->type() != "block-settings") :
-                                ?><?= $setting_block->blocksSpace() ?><?php
-                            endif;
-                        endif ?>"
-            >
-                    <?= $block ?>
-            </div>
-                <?php endif; ?>
-            <?php endforeach ?>
-        </div>
-    <?php endforeach ?>
-    </div>
-</section>
+                    if (preg_match($pattern, $uri, $matches)) {
+                        echo("<" . $matches[1] . $matches[2] . $color_style . ">" . $matches[3] . "</" . $matches[1] . ">");
+                    }
+            ?></div>
+            <?php
+                    endif;
+            endforeach ?></div>
+    <?php endforeach
+    ?>  </div>
+    </section>
 <?php endforeach ?>
 
 <script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 
-/* Switch color value according to your theme */
-const color1 = "#1f2937";
-const color2 = "#4f46e5";
-const color3 = "#0ea5e9";
-const color4 = "#72e2ca";
-const color5 = "#e0e7ff";
-
-/* Add more color available by creating a new variable and adding it to this array according to the syntax */
-const customColors = {
-  [color1]: color1,
-  [color2]: color2,
-  [color3]: color3,
-  [color4]: color4,
-  [color5]: color5,
-};
-
 module.exports = {
   content: [],
   theme: {
     extend: {
-      colors: {
-        ...customColors,
-      },
       spacing: {
         '320': '80rem',
         '240': '60rem',
@@ -34,13 +15,6 @@ module.exports = {
   safelist: [
     {
       pattern: /^mb-(4|8|16)$/,
-    },
-    {
-      pattern: new RegExp(`bg-(${Object.keys(customColors).join('|')})$`),
-    },
-    {
-      pattern: new RegExp(`text-(${Object.keys(customColors).join('|')})$`),
-      variants: ['[&_*]'],
     },
     {
       pattern: /^m(l-0|r-0|x-auto)$/
